53c44528c09b9b46babdd9d0ab2b4028001737d7
Engineerbabu777 <awaismumtaz0099@gmail.com>
2025-03-21 10:25:00 +0500

    `Welcome Text Added on Login Screen`

--- app/login.tsx
+++ app/login.tsx
@@ -1,7 +1,9 @@
-import { View, Text, StatusBar } from "react-native";
+import { View, Text, StatusBar, StyleSheet, TextInput } from "react-native";
 import React from "react";
 import ScreenWrapper from "@/components/ScreenWrapper";
 import BackButton from "@/components/BackButton";
+import { hp, wp } from "@/helpers/common";
+import { theme } from "@/constants/theme";
 
 type Props = {};
 
@@ -10,11 +12,56 @@ const login = (props: Props) => {
     <ScreenWrapper bg={"white"}>
       <StatusBar barStyle={"dark-content"} />
 
-      <View>
+      <View style={styles.container}>
         <BackButton />
+
+        {/* welcome */}
+        <View>
+          <Text style={styles.welcomeText}>Hey,</Text>
+          <Text style={styles.welcomeText}>Welcome Back</Text>
+        </View>
+
+        {/* form! */}
+        <View style={styles.form}>
+          <Text
+            style={{
+              fontSize: hp(1.5),
+              color: theme.colors.text
+            }}
+          >
+            Please login to continue
+          </Text>
+        </View>
       </View>
     </ScreenWrapper>
   );
 };
 
 export default login;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    gap: 45,
+    paddingHorizontal: wp(5)
+  },
+  welcomeText: {
+    fontSize: hp(4),
+    fontWeight: "bold",
+    color: theme.colors.text
+  },
+  form: {
+    gap: 25
+  },
+  forgotPassword: {
+    textAlign: "right",
+    fontWeight: "700",
+    color: theme.colors.text
+  },
+  footer: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    gap: 5
+  }
+});
