90011795a3d8d7c65b9d0b12da88fa8dd7f4081c
Engineerbabu777 <awaismumtaz0099@gmail.com>
2025-03-23 11:58:01 +0500

    `Now we can add images to create post`

--- app/(main)/newPost/index.tsx
+++ app/(main)/newPost/index.tsx
@@ -1,4 +1,11 @@
-import { ScrollView, StyleSheet, Text, View } from "react-native";
+import {
+  Pressable,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View
+} from "react-native";
 import React, { useRef, useState } from "react";
 import ScreenWrapper from "@/components/ScreenWrapper";
 import Header from "@/components/Header";
@@ -7,6 +14,12 @@ import { theme } from "@/constants/theme";
 import Avatar from "@/components/Avatar";
 import { useAuth } from "@/context/AuthContext";
 import RichTextEditor from "@/components/RichTextEditor";
+import { Icon } from "@/assets/icons";
+import Button from "@/components/Button";
+import * as ImagePicker from "expo-image-picker";
+import { Image } from "expo-image";
+import { getSupabaseFileUrl } from "@/services/image.service";
+import { Video } from "expo-av";
 
 export default function index() {
   const { user } = useAuth();
@@ -15,7 +28,61 @@ export default function index() {
   const editorRef = useRef("");
 
   const [loading, setLoading] = useState(false);
-  const [files, setFiles] = useState([]);
+  const [file, setFile] = useState(null);
+
+  const onPick = async (isImage) => {
+    let mediaConfig = {
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 0.7
+    };
+
+    if (!isImage) {
+      mediaConfig = {
+        mediaTypes: ImagePicker.MediaTypeOptions.Videos,
+        allowsEditing: true
+      };
+    }
+
+    let result = await ImagePicker.launchImageLibraryAsync(mediaConfig);
+
+    if (!result.canceled) {
+      setFile(result.assets[0]);
+    }
+  };
+  const onSubmit = async () => {};
+
+  const isLocalFile = (file) => {
+    if (!file) return null;
+
+    if (typeof file === "object") return true;
+    return false;
+  };
+  const getFileType = (file) => {
+    if (!file) return null;
+
+    if (isLocalFile(file)) {
+      return file.type;
+    }
+
+    if (file.includes("postImage")) {
+      return "image";
+    } else {
+      return "video";
+    }
+  };
+
+  const getFileUri = (file) => {
+    if (!file) return null;
+
+    if (isLocalFile(file)) {
+      return file.uri;
+    }
+
+    return getSupabaseFileUrl(file)?.uri;
+  };
+
   return (
     <ScreenWrapper bg={"white"}>
       <View style={styles.container}>
@@ -41,7 +108,54 @@ export default function index() {
               onChange={(body) => (bodyRef.current = body)}
             />
           </View>
+
+          {file && (
+            <>
+              <View style={styles.file}>
+                {getFileType(file) === "video" ? (
+                  <></>
+                ) : (
+                  <>
+                    <Image
+                      source={{ uri: getFileUri(file) }}
+                      resizeMode="cover"
+                      style={{ flex: 1 }}
+                    />
+                  </>
+                )}
+
+                <Pressable
+                  style={styles.closeIcon}
+                  onPress={() => setFile(null)}
+                >
+                  <Icon name="delete" color={"white"} size={20} />
+                </Pressable>
+              </View>
+            </>
+          )}
+
+          <View style={styles.media}>
+            <Text style={styles.addImageText}>Add to your post</Text>
+            <View style={styles.mediaIcons}>
+              <TouchableOpacity style={{}} onPress={() => onPick(true)}>
+                <Icon name={"image"} size={30} color={theme.colors.dark} />
+              </TouchableOpacity>
+              <TouchableOpacity style={{}} onPress={() => onPick(true)}>
+                <Icon name={"video"} size={30} color={theme.colors.dark} />
+              </TouchableOpacity>
+            </View>
+          </View>
         </ScrollView>
+
+        <Button
+          buttonStyle={{
+            height: hp(6.2)
+          }}
+          title="Post"
+          loading={loading}
+          hasShadow={false}
+          onPress={onSubmit}
+        />
       </View>
     </ScreenWrapper>
   );
@@ -53,6 +167,13 @@ const styles = StyleSheet.create({
     marginBottom: 30,
     paddingHorizontal: wp(4),
     gap: 15
+  },
+  file: {
+    height: hp(30),
+    width: "100%",
+    borderRadius: theme.radius.xl,
+    overflow: "hidden",
+    borderCurve: "continuous"
   },
   title: {
     fontSize: hp(2.5),
@@ -75,7 +196,19 @@ const styles = StyleSheet.create({
   },
   textEditor: {},
   media: {
-    flexDirection: "row"
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    borderWidth: 1.5,
+    padding: 12,
+    paddingHorizontal: 18,
+    borderRadius: theme.radius.xl,
+    borderColor: theme.colors.gray
+  },
+  mediaIcons: {
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 15
   },
   header: {
     gap: 12,
@@ -86,5 +219,18 @@ const styles = StyleSheet.create({
     fontSize: hp(1.5),
     fontWeight: "500",
     color: theme.colors.textLight
+  },
+  addImageText: {
+    fontSize: hp(1.9),
+    color: theme.colors.text,
+    fontWeight: "600"
+  },
+  closeIcon: {
+    position: "absolute",
+    top: 10,
+    right: 10,
+    backgroundColor: "rgba(255,0,0,0.6)",
+    padding: 7,
+    borderRadius: 50
   }
 });
